b2d57b98048b23bd34d8f2a5291d0928cefd2068
Lou Andria <lou@hei.school>
2023-11-02 00:43:23 +0100

    fix: save conf after gen

--- poja/core.py
+++ poja/core.py
@@ -3,6 +3,7 @@ import shutil
 import poja.sed as sed
 from poja.myrich import print_title, print_normal, print_banner
 from poja.version import get_version
+import yaml
 
 GIT_URL = "https://github.com/hei-school/poja"
 GIT_TAG = "v1.0.0"
@@ -33,7 +34,24 @@ def gen(app_name, region, ssm_sg_id, ssm_subnet1_id, ssm_subnet2_id, output_dir=
     print_normal("ssm_subnet2_id")
     sed.find_replace(temp_dir, "<?ssm-param-name-subnet2-id>", ssm_subnet2_id, exclude)
 
+    print_title("Save conf..")
+    save_conf(temp_dir, app_name, region, ssm_sg_id, ssm_subnet1_id, ssm_subnet2_id)
+    print_normal("poja.yml")
+
     print_title("Copy to output dir..")
     shutil.copytree(temp_dir, output_dir, dirs_exist_ok=True)
 
     print_title("... all done!")
+
+
+def save_conf(temp_dir, app_name, region, ssm_sg_id, ssm_subnet1_id, ssm_subnet2_id):
+    conf = {
+        "cli_version": get_version(),
+        "app_name": app_name,
+        "region": region,
+        "ssm_sg_id": ssm_sg_id,
+        "ssm_subnet1_id": ssm_subnet1_id,
+        "ssm_subnet2_id": ssm_subnet2_id,
+    }
+    with open(temp_dir + "/poja.yml", "w") as file:
+        yaml.dump(conf, file)

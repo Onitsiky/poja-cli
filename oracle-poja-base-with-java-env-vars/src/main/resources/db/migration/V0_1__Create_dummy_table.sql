bb7679a72c9e14e92bd47a887cf91b95741a1d76
Lou Andria <lou@hei.school>
2023-11-09 14:33:10 +0100

    feat: rm writing permission on generated files

--- oracle-poja-base-with-java-env-vars/src/main/resources/db/migration/V0_1__Create_dummy_table.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists dummy
-(
-    id varchar
-        constraint dummy_pk primary key
-);
-
-insert into dummy (id)
-values ('dummy-table-id-1')
-on conflict on constraint dummy_pk do nothing;
--- oracle-poja-base-without-postgres/src/main/resources/db/migration/V0_2__Create_dummy_uuid_table.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists dummy_uuid
-(
-    id varchar
-        constraint dummy_uuid_pk primary key
-);
-
-insert into dummy_uuid (id)
-values ('dummy-uuid-id-1')
-on conflict on constraint dummy_uuid_pk do nothing;

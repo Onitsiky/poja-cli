a44ca790a8f6fb709c9036ac86839ddf751c9283
Onitsiky <hei.onitsiky@gmail.com>
2023-11-17 11:30:45 +0300

    fix: read directly version from gradle.properties

--- poja/core.py
+++ poja/core.py
@@ -9,7 +9,7 @@ import yaml
 import os
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "9595c8a"
+GIT_TAG_OR_COMMIT = "bd59fa8"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

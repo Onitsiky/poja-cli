3057d3e1afd434be836f00123ab2b7e3545dd1c0
Onitsiky <hei.onitsiky@gmail.com>
2023-11-24 08:01:37 +0300

    feat: store eventBridge bus arn and Deadletter queue arn in SSM

--- poja/core.py
+++ poja/core.py
@@ -9,7 +9,7 @@ import yaml
 import os
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "8263aaf"
+GIT_TAG_OR_COMMIT = "1503a10"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

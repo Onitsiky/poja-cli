6e68f8b346291f9a211d406d5685a2532bdd02f4
Mahefaa <hei.mahefa@gmail.com>
2023-12-14 15:06:17 +0300

    fix: configurable region in deploy event and compute permission workflows

--- poja/core.py
+++ poja/core.py
@@ -12,7 +12,7 @@ import platform
 from pathlib import Path
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "b360171"
+GIT_TAG_OR_COMMIT = "18ccd67"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

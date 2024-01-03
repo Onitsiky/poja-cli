81386c26edbb9c2820f335fed389fe480c9ad799
Onitsiky <hei.onitsiky@gmail.com>
2024-01-03 14:56:53 +0300

    fix: specify region for S3AsyncClient bean

--- poja/core.py
+++ poja/core.py
@@ -12,7 +12,7 @@ import platform
 from pathlib import Path
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "33329ba"
+GIT_TAG_OR_COMMIT = "35630b0"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

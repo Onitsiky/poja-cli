2cba98f9f256f8051ed6db029e06d32312468a81
Onitsiky <hei.onitsiky@gmail.com>
2024-01-08 21:53:19 +0300

    feat: allow sending binaries in request and fix Instant format

--- poja/core.py
+++ poja/core.py
@@ -13,7 +13,7 @@ from poja.myos import cd_then_exec
 from pathlib import Path
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "35630b0"
+GIT_TAG_OR_COMMIT = "096d223"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

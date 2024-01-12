3d9825047ac275cdc734ee79b4c77b5c91c3fb0a
Onitsiky <hei.onitsiky@gmail.com>
2024-01-12 09:02:15 +0300

    fix: inherit secrets from parent in check-health after deploy

--- poja/core.py
+++ poja/core.py
@@ -13,7 +13,7 @@ from poja.myos import cd_then_exec
 from pathlib import Path
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "3c5ab57"
+GIT_TAG_OR_COMMIT = "92eec46"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

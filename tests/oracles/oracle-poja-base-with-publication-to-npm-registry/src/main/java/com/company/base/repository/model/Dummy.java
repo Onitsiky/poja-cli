f56948dc77739fa7fbc6f75118dfd68c4c664210
Onitsiky <hei.onitsiky@gmail.com>
2024-02-09 08:55:04 +0300

    feat!: upgrade spring boot to version 3
    BREAKING CHANGE: se sversion

--- tests/oracles/oracle-poja-base-with-publication-to-npm-registry/src/main/java/com/company/base/repository/model/Dummy.java
+++ tests/oracles/oracle-poja-base-with-publication-to-npm-registry/src/main/java/com/company/base/repository/model/Dummy.java
@@ -1,8 +1,8 @@
 package com.company.base.repository.model;
 
 import com.company.base.PojaGenerated;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 

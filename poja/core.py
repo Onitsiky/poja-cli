abd0dc33cef27bb17488ac3db15d923c55d96978
Lou Andria <lou@hei.school>
2024-02-11 20:14:59 +0100

    feat: upgrade tika from v1 to v2
    feat: add test-implem junit-pioneer

--- poja/core.py
+++ poja/core.py
@@ -13,7 +13,7 @@ from poja.myos import cd_then_exec
 from pathlib import Path
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "a823bc5"
+GIT_TAG_OR_COMMIT = "e038ecc"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

7c519b4bc0f7fea09e3b39182e197ee797b04ffd
Lou Andria <lou@hei.school>
2024-02-26 16:56:04 +0100

    fix: consume events with parallel workers configured by workers.thread.number that defaults to 5

--- poja/core.py
+++ poja/core.py
@@ -13,7 +13,7 @@ from poja.myos import cd_then_exec
 from pathlib import Path
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "af7a385"
+GIT_TAG_OR_COMMIT = "5cdf405"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"

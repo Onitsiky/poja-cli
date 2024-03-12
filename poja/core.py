06b999fbd5dd50b342921a27441d0064b260a728
Onitsiky <hei.onitsiky@gmail.com>
2024-03-12 20:04:30 +0300

    feat: add user tag on resources stacks

--- poja/core.py
+++ poja/core.py
@@ -14,7 +14,7 @@ from poja.myos import cd_then_exec
 from pathlib import Path
 
 GIT_URL = "https://github.com/hei-school/poja"
-GIT_TAG_OR_COMMIT = "bddc98c"
+GIT_TAG_OR_COMMIT = "3743e1d"
 
 DEFAULT_GROUP_NAME = "school.hei"
 DEFAULT_PACKAGE_FULL_NAME = DEFAULT_GROUP_NAME + ".poja"
